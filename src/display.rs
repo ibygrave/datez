3590c15fb041108dc30900c6ba4a8c869e13a52e
Ian Bygrave <ian@bygrave.me.uk>
2025-05-30 19:01:32 +0100

    slim display

--- src/display.rs
+++ src/display.rs
@@ -2,14 +2,8 @@ use bevy_ecs::system::Query;
 
 use crate::components::{DisplayLabel, SpanParts, TotalDays};
 
-pub(crate) fn print_total_days(query: Query<(&DisplayLabel, &TotalDays)>) {
-    for (label, days) in &query {
-        println!("{label}: {days}");
-    }
-}
-
-pub(crate) fn print_span_parts(query: Query<(&DisplayLabel, &SpanParts)>) {
-    for (label, parts) in &query {
-        println!("{label}: {parts}");
+pub(crate) fn print_total_days(query: Query<(&DisplayLabel, &TotalDays, &SpanParts)>) {
+    for (label, days, parts) in &query {
+        println!("{label}: {days} ago ({parts})");
     }
 }
--- src/plugins.rs
+++ src/plugins.rs
@@ -1,9 +1,9 @@
-use bevy_app::{App, Plugin, Update};
+use bevy_app::{App, Plugin, PostUpdate, Update};
 use bevy_ecs::schedule::IntoScheduleConfigs;
 
 use crate::{
     components::{update_elapsed_since, update_span_parts, update_total_days},
-    display::{print_span_parts, print_total_days},
+    display::print_total_days,
 };
 
 pub(crate) struct DatezPlugin;
@@ -12,14 +12,8 @@ impl Plugin for DatezPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            (
-                update_elapsed_since,
-                update_total_days,
-                update_span_parts,
-                print_total_days,
-                print_span_parts,
-            )
-                .chain(),
+            (update_elapsed_since, update_total_days, update_span_parts).chain(),
         );
+        app.add_systems(PostUpdate, print_total_days);
     }
 }
